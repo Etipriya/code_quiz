e4b27e4a605bdc0979153aa74740343ef64b7660
Eti Priya <etipriya@gmail.com>
2021-03-24 14:19:08 +0000

    add timer function

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,33 @@
 
 // Declare a click event on high score
 
+//Declare the start of timer from 60sec
+const timeEL = document.querySelector(".time");
+const mainEL = document.querySelector("main");
+
+let secondsLeft = 59;
+
+const setTime = function(){
+  let isStartQuiz;
+  let isCountdown;
+  let isStop;
+  timeEL isStartQuiz = setInterval(setTime,60000);
+  timeEL isCountdown =setInterval (decrease, 60000-1);
+  timeEL isStop = setInterval(stop, 0);
+
+  const callback =function(){
+
+  }
+  const timerInterval = setInterval(callback,1000);
+}
+const sendMessage = function(){
+
+}
+setTime();
+
 //Declare an event when click on the start button
 startBtn.addEventListener("click", startQuiz);
 
-function startQuiz() {}
+function startQuiz() {
+  introSection.style.display="none";
+}
