a5a522b78fb662640cfd292a7b6642976376d39d
Eti Priya <etipriya@gmail.com>
2021-03-24 00:59:48 +0000

    add script.js file

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// Declare characters
+
+// Declare a click event on high score
+
+//Declare an event when click on the start button
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {}
