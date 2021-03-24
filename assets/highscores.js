e177cf7b488bee84d03b6c25ba36091947c36a31
Eti Priya <etipriya@gmail.com>
2021-03-24 16:02:00 +0000

    add functionality of clear button

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,1 +1,13 @@
-//declare
+//Declare under high score page
+// Clear commands
+
+function clearText() {
+  let clear;
+  clear = document.getElementById("clear");
+  if (clear.firstChild.nodeValue === click) {
+    clear.firstChild.nodeValue = "clear-screen";
+  } else {
+    clear.firstChild.nodeValue = "stay";
+  }
+}
+addEventListener("click", clearScore);
