69573cbc57536716542a0d38bd4e0ac0a4471dba
Eti Priya <etipriya@gmail.com>
2021-04-29 22:36:51 +0100

    clear local storage

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,10 +1,12 @@
 const main = document.getElementById("high-scores-container");
+const clearButton = document.getElementById("clear-highscores");
 
 const renderHighScores = () => {
   const highscores = getFromLocalStorage();
 
   const ul = document.createElement("ul");
   ul.setAttribute("class", "highscores");
+  ul.setAttribute("id", "highscores");
 
   const renderListItem = each => {
     console.log("each");
@@ -19,77 +21,15 @@ const renderHighScores = () => {
   main.append(ul);
 };
 
+const clearHighscores = () => {
+  localStorage.clear();
+
+  const highscores = document.getElementById("highscores");
+
+  if (highscores) {
+    highscores.remove();
+  }
+};
+clearButton.addEventListener("click", clearHighscores);
+
 window.addEventListener("load", renderHighScores);
-
-// //Declare global data
-// const goBackBtn = document.getElementById("go-back");
-// const clearBtn = document.getElementById("clear");
-
-// // get high score
-// const getFromLocalStorage = () => {
-//   const highScores = localStorage.getItem("highScores");
-//   if (highScores) {
-//     return highScores;
-//   } else {
-//     return [];
-//   }
-// };
-// //render high score
-// const renderHighScoresTable = highScores => {
-//   if (highScores.length === 0) {
-//     const resultEmpty = noResults();
-//     bodyElement.appendChild(resultEmpty);
-//   } else {
-//     const showResults = resultsTable();
-//     bodyElement.appendChild(showResults);
-//   }
-// };
-
-// // shows no result element
-// const noResults = () => {
-//   const noResultsDiv = document.createElement("div");
-//   noResultsDiv.setAttribute("id", "results-div");
-
-//   const noResultsH2 = document.createElement("h2");
-//   noResultsH2.textContent = "No results to show right now";
-
-//   noResultsDiv.appendChild(noResultsH2);
-
-//   return noResultsDiv;
-// };
-
-// //Form a table to show the results of the quiz
-// const resultsTable = () => {
-//   const tableDiv = document.createElement("div");
-//   tableDiv.setAttribute("id", "table");
-
-//   const resultDiv = document.createElement("div");
-//   resultDiv.setAttribute("id", "results");
-//   resultDiv.textContent = getFromLocalStorage();
-
-//   tableDiv.appendChild(resultsDiv);
-
-//   return tableDiv;
-// };
-
-// const onload = () => {
-//   const highScores = getFromLocalStorage();
-//   renderHighScoresTable(highScores);
-// };
-
-// //Declare function for clear button
-// const clear = () => {
-//   localStorage.clear();
-//   function clearText() {
-//     let clear;
-//     clear = document.getElementById("clear");
-//     if (clear.firstChild.nodeValue === click) {
-//       clear.firstChild.nodeValue = "clear-screen";
-//     } else {
-//       clear.firstChild.nodeValue = "stay";
-//     }
-//   }
-// };
-// goBackBtn.addEventListener("click", goBackBtn);
-// clearBtn.addEventListener("click", clearBtn);
-// //window.addEventListener("load", onload);
